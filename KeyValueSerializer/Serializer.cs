d0276014bc5cf6b7c64d55eabe9c8b200f325025
Seth Moore <stmoore1@gmail.com>
2014-04-20 18:12:38 -0400

    Initial commit.

---
+++ KeyValueSerializer/Serializer.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using FoundationDB.Client;
+
+namespace FoundationDbSerializer
+{
+    public class Serializer
+    {
+        private static IList<PropertyInfo> FindKeyProperties<T>()
+        {
+            IList<PropertyInfo> keys = new List<PropertyInfo>();
+
+            // First search through the properties
+            foreach (var prop in typeof(T).GetProperties())
+                foreach (var attrib in prop.CustomAttributes)
+                    if (attrib.AttributeType == typeof(Key))
+                        keys.Add(prop);
+            return keys;
+        }
+
+        private static IList<FieldInfo> FindKeyFields<T>()
+        {
+            IList<FieldInfo> keys = new List<FieldInfo>();
+
+            foreach (var field in typeof(T).GetFields())
+                foreach (var attrib in field.CustomAttributes)
+                    if (attrib.AttributeType == typeof(Key))
+                        keys.Add(field);
+
+            return keys;
+        }
+
+        private static IEnumerable<KeyValuePair<string,string>> ConvertToKeyValuePairs<T>(IEnumerable<T> values)
+        {
+            // Get a collection of all the key properties
+            IList<PropertyInfo> pKeys = FindKeyProperties<T>();
+
+            // Get a collection of all the key fields
+            IList<FieldInfo> fKeys = FindKeyFields<T>();
+
+            // Make sure we have at least 1 key
+            if ((pKeys.Count + fKeys.Count) < 1)
+                throw new KeyNotFoundException("No properties or fields with the Key attribute were found in " + typeof(T).FullName);
+
+            // Get a collection of all the storable properties (writable primitives and strings)
+            IEnumerable<PropertyInfo> pVals = typeof(T).GetProperties().Where(p => p.CanWrite && (p.PropertyType.IsPrimitive || p.PropertyType == typeof(string)));
+
+            // Get a collection of all the storable fields (writable primitives and strings)
+            IEnumerable<FieldInfo> fVals = typeof(T).GetFields().Where(f => (f.FieldType.IsPrimitive || f.FieldType == typeof(string)));
+
+            List<KeyValuePair<string, string>> results = new List<KeyValuePair<string, string>>();
+
+            // Iterate through the values passed in, serializing them out to key/value pairs
+            foreach (var item in values)
+            {
+                // Build the key
+                StringBuilder key = new StringBuilder("");
+                foreach (var k in pKeys)
+                    key.Append(k.GetValue(item));
+                foreach (var k in fKeys)
+                    key.Append(k.GetValue(item));
+
+                // Build the value
+                // This serialization should be improved by wrapping the value to support commas and semicolons.
+                StringBuilder value = new StringBuilder("");
+                foreach (var v in pVals)
+                {
+                    value.AppendFormat("{0},{1};", v.Name, v.GetValue(item));
+                }
+                foreach (var v in fVals)
+                {
+                    value.AppendFormat("{0},{1};", v.Name, v.GetValue(item));
+                }
+
+                results.Add(new KeyValuePair<string,string>(key.ToString(),value.ToString()));
+            }
+
+            return results;
+        }
+
+        /// <summary>
+        /// Writes a collection of objects with at least one Key field into the 
+        /// foundationDB key/value store as a document containing the primitives of that object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="objects">The object to be serialized</param>
+        public async static void Write<T>(IEnumerable<T> objects)
+        {
+            var keyValuePairs = ConvertToKeyValuePairs(objects);
+
+            using (var db = await Fdb.OpenAsync())
+            {
+                // We'll use a subspace with the same name as the class including the namespace
+                var location = db.Partition(typeof(T).FullName);
+
+                // We need a transaction to be able to make changes to the db
+                using (var trans = db.BeginTransaction(System.Threading.CancellationToken.None))
+                {
+                    // Loop over each pair and throw it into the transaction
+                    foreach (var pair in keyValuePairs)
+                    {
+                        trans.Set(location.Pack(pair.Key), GetSlice(pair.Value));
+                    }
+                    
+                    // Commit the changes to the db
+                    await trans.CommitAsync();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retrieves an object with the given Key from the foundationDB key/value store.
+        /// Note that this has no support yet for handling null fields/properties.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="result"></param>
+        public async static Task<T> Read<T>(string key)
+        {
+            List<KeyValuePair<string, string>> keyValuePairs = new List<KeyValuePair<string, string>>();
+
+            using (var db = await Fdb.OpenAsync())
+            {
+                // We'll use a subspace with the same name as the class including the namespace
+                var location = db.Partition(typeof(T).FullName);
+
+                using (var trans = db.BeginTransaction(System.Threading.CancellationToken.None))
+                {
+                    Slice value = await trans.GetAsync(location.Pack(key));
+                    if (value.HasValue)
+                        keyValuePairs.Add(new KeyValuePair<string, string>(key, value.ToString()));
+                }
+            }
+
+            return ConvertToObject<T>(keyValuePairs).First();
+        }
+
+        private static Slice GetSlice(object value)
+        {
+            // For now we'll write everything out as a string to prevent excessive casting (there's also not a slice method for floats)
+            if (value is double)
+                return Slice.FromString(((double)value).ToString("R"));
+            if (value is float)
+                return Slice.FromString(((float)value).ToString("R"));
+
+            return Slice.FromString(value.ToString());
+        }
+
+        private static IEnumerable<T> ConvertToObject<T>(IEnumerable<KeyValuePair<string, string>> pairs)
+        {
+            // Get a collection of all the storable properties (writable primitives and strings)
+            Dictionary<string, PropertyInfo> properties = new Dictionary<string, PropertyInfo>();
+            foreach (var prop in typeof(T).GetProperties().Where(p => p.CanWrite && 
+                (p.PropertyType.IsPrimitive || p.PropertyType == typeof(string))))
+            {
+                properties.Add(prop.Name, prop);
+            }
+
+            // Get a collection of all the storable fields (writable primitives and strings)
+            Dictionary<string, FieldInfo> fields = new Dictionary<string, FieldInfo>();
+            foreach (var field in typeof(T).GetFields().Where(f => (f.FieldType.IsPrimitive || f.FieldType == typeof(string))))
+            {
+                fields.Add(field.Name, field);
+            }
+
+            List<T> results = new List<T>();
+
+            foreach (var pair in pairs)
+            {
+                T result = (T)FormatterServices.GetUninitializedObject(typeof(T));
+                string[] values = pair.Value.Split(';');
+
+                foreach (var value in values)
+                {
+                    var splitValue = value.Split(',');
+
+                    if (properties.ContainsKey(splitValue[0]))
+                        properties[splitValue[0]].SetValue(result, Convert.ChangeType(splitValue[1], properties[splitValue[0]].PropertyType));
+
+                    if (fields.ContainsKey(splitValue[0]))
+                        fields[splitValue[0]].SetValue(result, Convert.ChangeType(splitValue[1], fields[splitValue[0]].FieldType));
+                }
+                results.Add(result);
+            }
+
+            return results;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class Key : Attribute
+    {}
+}
---
+++ foundationDbTest/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using FoundationDbSerializer;
+
+namespace foundationDbTest
+{
+    class User
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string First;
+        public string Last;
+
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                new MailAddress(value);
+                _email = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} - {1} {2} ({3})", ID, First, Last, Email);
+        }
+
+        public bool Equals(User user)
+        {
+            return First == user.First && Last == user.Last && ID == user.ID && Email == user.Email;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> users = new List<User>();
+            users.Add(new User() { ID = 1, First = "Bob", Last = "Smith", Email = "bob@smith.com" });
+            users.Add(new User() { ID = 2, First = "Jim", Last = "Smith", Email = "jim@smith.com" });
+            users.Add(new User() { ID = 3, First = "Ed", Last = "Smith", Email = "ed@smith.com" });
+
+            Serializer.Write(users);
+
+            Read();
+
+            Console.ReadLine();
+        }
+        
+        static async void Read()
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine(await Serializer.Read<User>(i.ToString()));
+            }
+        }
+    }
+}
